4f3c93eeae38eb0cf9830905a3dbec36fb305224
Bhushan-Prakash-Bhamare <Bhushubhamare@gmail.com>
2023-06-25 13:05:19 +0530

    user authentication task-8

--- public/js/addexpense.js
+++ public/js/addexpense.js
@@ -2,41 +2,43 @@ const exForm=document.getElementById('expform');
 const displayList=document.querySelector('.list-group');
 exForm.addEventListener('submit',formSubmit);
  
-function formSubmit(e)
-{   
+async function formSubmit(e)
+{   try{
     e.preventDefault();
     const amount=document.getElementById('expamt').value;
     const description=document.getElementById('desc').value;
     const category=document.getElementById('sel-list').value;
+    const token=localStorage.getItem('token');
+
     let myobj={
       amount,description,category
     }
-    axios
-      .post("http://localhost:3100/expense/add-expense",myobj)
-      .then(response=>{
-        console.log(response);
+        const response=await axios.post("http://localhost:3100/expense/add-expense",myobj,{headers:{"Authorization":token}})
         showExp(response.data.newExpenseData);
-      })
-      .catch(err=>{
+      
+    }
+    catch(err){
         document.body.innerHTML=document.body.innerHTML+"<h4>Something went wrong</h4>"    
-        console.log(err)});
-
-    
+        console.log(err)
+    };   
 }
 
-window.addEventListener("DOMContentLoaded",()=>{
-    axios
-        .get("http://localhost:3100/expense")
-        .then((response)=>{
-            for(var i=0;i<response.data.expenseData.length;i++)
-              showExp(response.data.expenseData[i]);
-        })
-        .catch((error)=>console.log(error));
+window.addEventListener("DOMContentLoaded",async()=>{
+   try{
+    const token=localStorage.getItem('token');
+    const response=await axios.get("http://localhost:3100/expense",{ headers:{"Authorization":token}})
+    for(var i=0;i<response.data.expenseData.length;i++)
+        showExp(response.data.expenseData[i]);
+    }
+    catch(error){
+        console.log(error)
+    };
 })
 
-function showExp(myobj)
+async function showExp(myobj)
 {
-    const addNewelem=document.createElement('li');
+    try{
+        const addNewelem=document.createElement('li');
     addNewelem.className="list-group-item bg-light";
     const text=document.createTextNode(myobj.amount+"-"+myobj.description+"-"+myobj.category);
     addNewelem.appendChild(text);
@@ -48,14 +50,19 @@ function showExp(myobj)
 
     displayList.appendChild(addNewelem);
 
-    deletebtn.addEventListener('click',function(){
-        const dId=myobj.id;
-          axios
-            .delete(`http://localhost:3100/expense/delete-expense/${dId}`)
-            .then(()=>{
+    deletebtn.addEventListener('click',async function(){
+        try{
+            const token=localStorage.getItem('token');
+            const dId=myobj.id;
+            await axios.delete(`http://localhost:3100/expense/delete-expense/${dId}`,{headers:{"Authorization":token}})
                 displayList.removeChild(addNewelem);
-            })
-            .catch((err)=>console.log(err));
-           
-      })
+        }
+        catch(err){
+            console.log(err)
+        };     
+    })
+    }
+    catch(error){
+        console.log(error)
+    };
 }

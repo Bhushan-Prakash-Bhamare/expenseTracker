8f97f626053b2a638999b5b3d455fa182ac18971
Bhushan-Prakash-Bhamare <Bhushubhamare@gmail.com>
2023-07-05 09:18:48 +0530

    location changed

--- public/js/login.js
+++ public/js/login.js
@@ -30,7 +30,7 @@ async function formSubmit(e){
       const response=await axios.post(`http://13.51.204.19:3100/user/login`,details);  
           console.log(response.data.message); 
           localStorage.setItem('token',response.data.token);
-          window.location.href = "http://127.0.0.1:5500/views/addexpense.html";  
+          window.location.href = "http://127.0.0.1:5500/public/html/addexpense.html";  
       
   }
   catch(err){ 
